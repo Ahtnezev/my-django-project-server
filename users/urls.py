8de64c451eac9fcd77e6187cfcd1b2bae4664a90
uneven74 <ahtnezev@gmail.com>
2025-08-27 22:30:14 -0600

    [add] Login fn

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create
+from .views import create, login
 
 # http://127.0.0.1:8000/api/users on postman
 # http://127.0.0.1:8000/users on postman (new)
 
 urlpatterns = [
-    path('', create)
+    path('', create),
+    path('/login', login)
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+import bcrypt
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+from .models import User
 from users.serializers import UserSerializer
 
 # create a custom python environment -> venv
@@ -24,3 +26,29 @@ def create(request):
         serializer.save() # Save in database
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    if not email or not password:
+        return Response({"error": "El email y password son obligatorios"}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({"error": "Email o password no son correctos"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+    if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        user_data = {
+            "id": user.id,
+            "name": user.name,
+            "lastname": user.lastname,
+            "email": user.email,
+            "phone": user.phone,
+            "image": user.image,
+            "notification_token": user.notification_token
+        }
+        return Response(user_data, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Email o password no son correctos"}, status=status.HTTP_401_UNAUTHORIZED)

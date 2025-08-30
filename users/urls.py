8a8bf4b3397482a3472661c2b94b4eeb4cd4c927
uneven74 <ahtnezev@gmail.com>
2025-08-29 22:49:56 -0600

    [update] user update

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-# from .views import create, login
-
-# http://127.0.0.1:8000/api/users on postman
-# http://127.0.0.1:8000/users on postman (new)
+from .views import update
 
 urlpatterns = [
-    # path('', create),
+    path('/<id_user>', update),
     # path('/login', login)
 ]
